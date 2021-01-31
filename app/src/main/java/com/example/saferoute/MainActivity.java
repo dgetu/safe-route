7440d102d84454c204e9525445b79b9503f9d7bb
thewongian <thewongian@vt.edu>
2021-01-31 12:42:06 -0500

    Attempted heatmaps, but currently not working

--- app/src/main/java/com/example/saferoute/MainActivity.java
+++ app/src/main/java/com/example/saferoute/MainActivity.java
@@ -1,32 +1,61 @@
 package com.example.saferoute;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.SearchView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.TileOverlay;
+import com.google.android.gms.maps.model.TileOverlayOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.channels.Channels;
+import java.util.ArrayList;
 import java.util.List;
 
 //imports for heat map
+import com.google.cloud.ReadChannel;
 import com.google.cloud.bigquery.BigQuery;
 import com.google.cloud.bigquery.BigQueryOptions;
-import com.google.cloud.bigquery.FieldValueList;
 import com.google.cloud.bigquery.Job;
 import com.google.cloud.bigquery.JobId;
-import com.google.cloud.bigquery.JobInfo;
 import com.google.cloud.bigquery.QueryJobConfiguration;
-import com.google.cloud.bigquery.TableResult;
+import com.google.cloud.bigquery.Table;
+import com.google.cloud.bigquery.TableId;
+import com.google.maps.android.heatmaps.HeatmapTileProvider;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Scanner;
 import java.util.UUID;
 
 /**
@@ -38,6 +67,12 @@ import java.util.UUID;
  *
  */
 public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+    //Fields
+    //DEFAULT VALUES
+    /**
+     * Default location (Washington, DC)
+     */
+    private final LatLng defaultLocation = new LatLng(38.9072, -77.0369);
     /**
      * The default zoom value for the map
      */
@@ -45,15 +80,33 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
     /**
      * The maximum allowed results in a search query
      */
+    private static final String TAG = MainActivity.class.getSimpleName();
     private static final int MAX_RESULTS = 1;
-
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 1;
+    //map stuff
     GoogleMap map;
     SupportMapFragment mapFragment;
+    //search stuff
     SearchView searchView;
+
 
     // initializing a big query object
     BigQuery bigquery = BigQueryOptions.getDefaultInstance().getService();
 
+    //location stuff
+    private boolean locationPermissionGranted;
+
+    // The geographical location where the device is currently located. That is, the last-known
+    // location retrieved by the Fused Location Provider.
+    private Location lastKnownLocation;
+
+    // The entry point to the Fused Location Provider.
+    private FusedLocationProviderClient fusedLocationProviderClient;
+
+    //data stuff idk
+    private String projectId = "ornate-veld-161301";
+    private String datasetName = "crime_data_dc";
+    private String tableName = "data_2020";
     /**
      * Method that runs when the app first starts up
      * 
@@ -66,7 +119,11 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         setContentView(R.layout.activity_main);
 
         searchView = findViewById(R.id.sv_location);
-        mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.google_map);
+
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             /**
@@ -118,22 +175,27 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        map = googleMap;
 
-        LatLng dc = new LatLng(38.9072, -77.0369);
-        map.moveCamera(CameraUpdateFactory.newLatLng(dc));
-        map.moveCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM));
+        map = googleMap;
+        // Turn on My Location layer and the related control on map
+        updateLocationUI();
+        // Get the current location of the device and set potisition of the map
+        getDeviceLocation();
+
 
     }
 
+
     private void addHeatMap() {
+
         List<LatLng> latLngs = null;
 
         // Get the data: latitude/longitude positions of police stations.
         try {
-            latLngs = readItems( runQuery() );
-        } catch (JSONException e) {
-            Toast.makeText(context, "Problem reading list of locations.", Toast.LENGTH_LONG).show();
+            runQuery();
+            latLngs = readItems();
+        } catch (JSONException | InterruptedException e) {
+            Toast.makeText(getApplicationContext(), "Problem reading list of locations.", Toast.LENGTH_LONG).show();
         }
 
         // Create a heat map tile provider, passing it the latlngs of the police
@@ -144,9 +206,14 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         TileOverlay overlay = map.addTileOverlay(new TileOverlayOptions().tileProvider(provider));
     }
 
-    private List<LatLng> readItems(@RawRes int resource) throws JSONException {
+    private List<LatLng> readItems() throws JSONException {
         List<LatLng> result = new ArrayList<>();
-        InputStream inputStream = context.getResources().openRawResource(resource);
+
+        Storage storage = StorageOptions.newBuilder().setProjectId(projectId).build().getService();
+
+        Blob blob = storage.get(BlobId.of("ornate-veld-161301.appspot.com", "/data/json/data.json"));
+        ReadChannel channel = blob.reader();
+        InputStream inputStream = Channels.newInputStream(channel);
         String json = new Scanner(inputStream).useDelimiter("\\A").next();
         JSONArray array = new JSONArray(json);
         for (int i = 0; i < array.length(); i++) {
@@ -158,32 +225,140 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         return result;
     }
 
-    private Job runQuery() {
+    private Job runQuery() throws InterruptedException {
+
+
         QueryJobConfiguration queryConfig = QueryJobConfiguration
-                .newBuilder("SELECT X, Y " + "FROM 'ornate-veld-161301.crime_data_dc.data_2020' "
-                        + "ORDER BY subject DESC LIMIT 1000")
+                .newBuilder(String.format("SELECT X, Y FROM '%s.%s.%s' ORDER BY subject DESC LIMIT 1000"
+                    , projectId, datasetName, tableName))
                 // Use standard SQL syntax for queries.
                 // See: https://cloud.google.com/bigquery/sql-reference/
-                .setUseLegacySql(false).build();
+                    .setUseLegacySql(false).build();
 
         // Create a job ID so that we can safely retry.
         JobId jobId = JobId.of(UUID.randomUUID().toString());
-        Job queryJob = bigquery.create(JobInfo.newBuilder(queryConfig).setJobId(jobId).build());
+        TableId tableId = TableId.of(projectId, datasetName, tableName);
+        Table table = bigquery.getTable(tableId);
+
+        Job queryJob = table.extract("json", "gs://ornate-veld-161301.appspot.com/data/json/data.json");
 
         // Wait for the query to complete.
-        queryJob = queryJob.waitFor();
+        Job completedJob = queryJob.waitFor();
 
         // Check for errors
-        if (queryJob == null) {
+        if (completedJob == null) {
             throw new RuntimeException("Job no longer exists");
         } else if (queryJob.getStatus().getError() != null) {
             // You can also look at queryJob.getStatus().getExecutionErrors() for all
             // errors, not just the latest one.
             throw new RuntimeException(queryJob.getStatus().getError().toString());
         }
-        return queryJob;
+        return completedJob;
     }
 
     
+
+
+    /**
+     * request location perms so that we can get location of device
+     * result of the permission request is handled by a callback,
+     * onRequestPremissionsResult.
+     */
+    private void getLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            locationPermissionGranted = true;
+        }
+        else {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION},
+                    PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+        }
+
+
+    }
+
+    /**
+     *
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        locationPermissionGranted = false;
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    locationPermissionGranted = true;
+                }
+            }
+        }
+        updateLocationUI();
+    }
+
+    /**
+     * updates the location
+     */
+    private void updateLocationUI() {
+        if (map == null) {
+            return;
+        }
+        try {
+            if (locationPermissionGranted) {
+                map.setMyLocationEnabled(true);
+                map.getUiSettings().setMyLocationButtonEnabled(true);
+            } else {
+                map.setMyLocationEnabled(false);
+                map.getUiSettings().setMyLocationButtonEnabled(false);
+                lastKnownLocation = null;
+                getLocationPermission();
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage());
+        }
+    }
+
+    /**
+     * Get the best and most recent location of the device, which may be null in rare
+     * cases when a location is not available.
+     */
+    private void getDeviceLocation() {
+
+        try {
+            if (locationPermissionGranted) {
+                Task<Location> locationResult = fusedLocationProviderClient.getLastLocation();
+                locationResult.addOnCompleteListener(this, new OnCompleteListener<Location>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Location> task) {
+                        if (task.isSuccessful()) {
+                            // Set the map's camera position to the current location of the device.
+                            lastKnownLocation = task.getResult();
+                            if (lastKnownLocation != null) {
+                                map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                                        new LatLng(lastKnownLocation.getLatitude(),
+                                                lastKnownLocation.getLongitude()), DEFAULT_ZOOM));
+                                map.setMyLocationEnabled(true);
+                            }
+                        } else {
+                            Log.d(TAG, "Current location is null. Using defaults.");
+                            Log.e(TAG, "Exception: %s", task.getException());
+                            map.moveCamera(CameraUpdateFactory
+                                    .newLatLngZoom(defaultLocation, DEFAULT_ZOOM));
+                            map.getUiSettings().setMyLocationButtonEnabled(false);
+                        }
+                    }
+                });
+            }
+        } catch (SecurityException e)  {
+            Log.e("Exception: %s", e.getMessage(), e);
+        }
+    }
+
 
 }
